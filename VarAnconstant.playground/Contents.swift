890cbbe70794a40281763716a84edb6a9ed89472
akapan <akapan.r@hitachi-tstv.com>
2019-01-24 09:41:02 +0700

    Var and let

---
+++ VarAnconstant.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+//Variable and Constant
+//Implicit Variable
+var nameString = "Nardanong.s"
+var numberInt = 100
+var numberDob = 12.34
+var statusBol = true
+
+//Explicit Variable
+var surName: String = "Sukbua"
+var number2:Int = 500
+var number3:Double = 3.1416
+var status:Bool = false
+
+//CONSTANT
+let myName:String = "Doremon"
+
+nameString = "Nardanong.s12345"
+//myName = "Doremon1234" // Cannot Compile Error
+
+
+
+
+
+
+
+
+
+
+
+
